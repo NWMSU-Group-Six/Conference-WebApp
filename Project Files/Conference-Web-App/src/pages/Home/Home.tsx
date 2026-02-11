4711edeb18f79d4822329f94e4d06aab02e5b373
Tanner Jackley <tannerjackley@gmail.com>
2026-02-11 14:03:07 -0600

    installed Shadcn and tailwind, provided shadcn button examples for demonstration

--- Project Files/Conference-Web-App/src/pages/Home/Home.tsx
+++ Project Files/Conference-Web-App/src/pages/Home/Home.tsx
@@ -1,6 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 function Home() {
   return (
     <>
+      <h1>Examples of buttons:</h1>
+      <Button>Click Me</Button>
+      <Button size="xs">Click Me</Button>
+      <Button variant="secondary">Click Me</Button>
+      <Button variant="outline">Click Me</Button>
+      <Button variant="ghost">Click Me</Button>
       <h1>Hello Cat Coding!</h1>
       <p>Welcome to the conference web app.</p>
     </>
