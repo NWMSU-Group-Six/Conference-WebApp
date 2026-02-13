b06b0570bf7b196ffc6b3ab9ee971682535e2561
tannerjackley <s561664@nwmissouri.edu>
2026-02-12 21:43:28 -0600

    added Card component to page

--- Project Files/Conference-Web-App/src/pages/Speakers/Speakers.tsx
+++ Project Files/Conference-Web-App/src/pages/Speakers/Speakers.tsx
@@ -1,7 +1,17 @@
+import SpeakerCard from "@/components/custom/SpeakerCard";
+
 function Speakers() {
   return (
     <>
-      <h1>Speakers Page</h1>
+      <div className="text-3xl font-bold mb-8">Speakers</div>
+      <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+        <SpeakerCard />
+        <SpeakerCard />
+        <SpeakerCard />
+        <SpeakerCard />
+        <SpeakerCard />
+        <SpeakerCard />
+      </div>
     </>
   );
 }
