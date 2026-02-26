56c96a03f8de270f3abbd3c2a22e79871a2e3d20
Tanner Jackley <tannerjackley@gmail.com>
2026-02-26 15:22:01 -0600

    added connection to firebase and initialized auth and db

--- Project Files/Conference-Web-App/src/firebase/auth.ts
+++ Project Files/Conference-Web-App/src/firebase/auth.ts
@@ -1,1 +1,13 @@
 // Authorization and authentication logic using Firebase Authentication
+// Authorization and authentication logic using Firebase Authentication
+import { getAuth } from "firebase/auth";
+import { app } from "./firebase";
+// auth.js
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const auth = getAuth(app);
--- Project Files/Conference-Web-App/src/firebase/db.ts
+++ Project Files/Conference-Web-App/src/firebase/db.ts
@@ -1,1 +1,6 @@
 // Firebase database logic
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore";
+import { app } from "./firebase";
+
+const db = getFirestore(app);
