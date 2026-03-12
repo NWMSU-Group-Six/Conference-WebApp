c9f1a0f9ef436fd398c1bd265cca5045a9200ca6
Tanner Jackley <tannerjackley@gmail.com>
2026-03-12 14:34:02 -0500

    refactor speakers

--- src/firebase/db.ts
+++ src/firebase/db.ts
@@ -2,7 +2,9 @@
 import { getDocs, collection } from "firebase/firestore";
 import { db } from "./firebase";
 
-export const getDataByCollection = async (collectionName: string) => {
+export const getDataByCollection = async <T>(
+  collectionName: string,
+): Promise<T[]> => {
   try {
     console.log("Fetching collection:", collectionName);
     const querySnapshot = await getDocs(collection(db, collectionName));
@@ -13,7 +15,7 @@ export const getDataByCollection = async (collectionName: string) => {
     return querySnapshot.docs.map((doc) => ({
       id: doc.id,
       ...doc.data(),
-    }));
+    })) as T[];
   } catch (error) {
     console.error(`Error fetching ${collectionName} data: `, error);
     throw error;
